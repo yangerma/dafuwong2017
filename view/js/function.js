85e58bd97ee8de140a7e5286ee166aaa6eac51d2
w4a2y4 <b04902079@ntu.edu.tw>
2017-06-27 15:53:11 +0800

    sth noy important

--- view/js/function.js
+++ view/js/function.js
@@ -1,7 +1,43 @@
-function getPosition(id) {
-	var x = $(id).offset().left;
-	var y = $(id).offset().top;
-	alert('x:'+x+', y:'+y);
+var socket = io();
+function roll_dice() {
+    socket.emit('roll_dice');
+    return false;
+}
+
+socket.on('dice_result', function(res){
+    console.log('haha');
+    document.getElementById('output').innerHTML =
+       'user ' + res.player + ' got '+ res.dice_result;
+});
+
+socket.on('update', function( data ) {
+	var player = data.player;	
+	var rank = data.rank;
+
+	for (var i = 0; i < 5; i++) {
+
+		var currPos = '#' + player[i].pos;
+		var x = $(currPos).offset().left;
+		var y = $(currPos).offset().top;
+		$("#player" + i).css('top', y);
+		$("#player" + i).css('left', x);
+
+		//var j = rank[i];
+		var j = i;
+		$('#info' + (i+1) + ' h4').text(player[j].name);
+		$('#info' + (i+1) + ' p').text('$' + player[j].money);
+
+	}
+});
+
+function updatePosition() {
+	for (var i = 0; i < 5; i++) {
+		var currPos = '#' + player[i].pos;
+		var x = $(currPos).offset().left;
+		var y = $(currPos).offset().top;
+		$("#player" + i).css('top', y);
+		$("#player" + i).css('left', x);
+	}
 }
 
 function updateScoreBoard() {
@@ -13,12 +49,3 @@ function updateScoreBoard() {
 	}
 }
 
-function updatePosition() {
-	for (var i = 0; i < 5; i++) {
-		var currPos = '#' + player[i].pos;
-		var x = $(currPos).offset().left;
-		var y = $(currPos).offset().top;
-		$("#player" + i).css('top', y);
-		$("#player" + i).css('left', x);
-	}
-}
