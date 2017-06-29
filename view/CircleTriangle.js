cbce32cfdf549fda1c4b1bd92bb3cd34327e255d
w4a2y4 <b04902079@ntu.edu.tw>
2017-06-30 00:25:05 +0800

    Rewrite CircleTriangle

--- view/CircleTriangle.js
+++ view/CircleTriangle.js
@@ -1,29 +1,31 @@
-var tleft = ['50% - 40px', '100% - 40px', '80.86% - 40px', '19.14% - 40px', '0% - 40px' ];
-var ttop  = ['0% - 34.8px', '38.31% - 34.8px', '100% - 34.8px', '100% - 34.8px', '38.31% - 34.8px'];
 for (var i = 0; i < 5; i++) {
 
-	var iContent;
-	iContent = '<div class="triangle" id="t' + i + '"';
-	iContent += ' style="top:calc(' +ttop[i]+ '); left:calc(' +tleft[i]+ ');"></div>';
-	document.write(iContent);
+	var newtleft = [0.5, 1, 0.8086, 0.1914, 0];
+	var newttop  = [0, 0.3831, 1, 1, 0.3831];
+	var parentHeight = 700;
+	var parentWidth = 900;
+
+	document.write('<div class="triangle" id="t' + i + '"></div>');
+	$('#t'+i).css('left', newtleft[i]*parentWidth-40);
+	$('#t'+i).css('top', newttop[i]*parentHeight-34.8);
 
 	if( i==0 || i==2 || i==3 ) {
 		for (var j = 1; j < 4; j++) {
-			var sContent;
-			sContent = '<div class="circle" id="s' + i + '' + j + '"';
-			sContent += ' style="top:calc( 50% - 30.8px + 1/4 * ' +j+ ' * (' +ttop[i]+ ' - 50% + 30.8px ) );';
-			sContent += ' left:calc( 50% - 32.4px + 1/4 * ' +j+ ' * (' +tleft[i] + ' - 50% + 32.4px ) + 10px );"></div>';
-			document.write(sContent);
+			document.write('<div class="circle" id="s' + i + '' + j + '"></div>');
+			var topvalue = 0.5*parentHeight - 30.8 + 0.25*j*( ( newttop[i] - 0.5 ) * parentHeight - 4.8 );
+			var leftvalue = 0.5*parentWidth - 22.4 + 0.25*j*( ( newtleft[i] - 0.5 ) * parentWidth - 7.6 );
+			$('#s'+i+''+j).css('left', leftvalue);
+			$('#s'+i+''+j).css('top', topvalue);
 		}	
 	}
 
 	for (var j = 1; j < 6; j++) {
-		var jContent;
 		var nexti = (i+1)%5;
-		jContent = '<div class="circle" id="c' + i + '' + j + '"';
-		jContent += ' style="top:calc( '+ttop[i]+' + 1/6 * ' +j+ ' * (' +ttop[nexti]+ ' - (' +ttop[i]+ ')  ) + 8.8px );';
-		jContent += ' left:calc( '+tleft[i]+' + 1/6 * ' +j+ ' * (' +tleft[nexti]+ ' - (' +tleft[i] + ')  ) + 15px );"></div>';
-    	document.write(jContent);
+    	document.write('<div class="circle" id="c' + i + '' + j + '"></div>');
+    	var topvalue = newttop[i]*parentHeight - 34.8 + 1/6 * j * ( newttop[nexti]*parentHeight - 34.8 - ( newttop[i]*parentHeight - 34.8 )   ) + 8.8 ;
+    	var leftvalue = newtleft[i]*parentWidth - 40 + 1/6 * j * ( newtleft[nexti]*parentWidth - 40 - (newtleft[i]*parentWidth - 40 )  ) + 15;
+		$('#c'+i+''+j).css('left', leftvalue);
+		$('#c'+i+''+j).css('top', topvalue);
     }	
 
 }
