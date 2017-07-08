b4464cd9b5e10443a88672d5235a3cadc1f1ecac
w4a2y4 <b04902079@ntu.edu.tw>
2017-07-08 17:25:41 +0800

    Fix bugs after applying cpy_map

--- view/js/firewall.js
+++ view/js/firewall.js
@@ -13,13 +13,27 @@ function closeFirewall() {
 	$('#backpack').hide()
 	$('#firewallBox').hide();
 }
+
+
 function firewallPos(blockList) {
 
-	$('#map div').addClass('activeLand');	
+	timeToChooseLand = true;
+	$('#cpy_map div').addClass('activeLand');	
 
-	$('#map div').on('click', function() {
-		$('#map div').removeClass('activeLand');
+	var nodeID;
+	$("#cpy_map .activeLand").on("mouseover", function() {
+	    nodeID = $( this ).prop('id');
+	    nodeID = nodeID.substr(4);
+	    $('#'+nodeID).css('transform', 'scale(1.15, 1.15)');
+	}).on("mouseout", function() {
+		$('#'+nodeID).css('transform', 'scale(1, 1)');
+	});
+
+	$('#cpy_map div').on('click', function() {
+		$('#cpy_map div').removeClass('activeLand');
+		if ( !timeToChooseLand ) return;
 		var landID = $( this ).prop('id');
+		landID = landID.substr(4);
 		timeToChooseLand = false;
 		socket.emit('buy_item', playerId, 'firewall', {pos: landID, blockList: blockList});
 	});
