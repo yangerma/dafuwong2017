809e420952d79461e8258749f3bea9e8f744ef85
w4a2y4 <b04902079@ntu.edu.tw>
2017-07-08 16:25:33 +0800

    Fix switch bigs after apply cpy_map

--- view/js/switch.js
+++ view/js/switch.js
@@ -18,12 +18,21 @@ function chooseLand() {
 
 	$('#switchBox').hide();
 	timeToChooseLand = true;
-	$('#map div').addClass('activeLand');	
-	$('#map .inner').removeClass('activeLand');
+	$('#cpy_map div').addClass('activeLand');	
+	$('#cpy_map .inner').removeClass('activeLand');
 
-	$('#map div').on('click', function() {
-		$('#map .inner').addClass('activeLand');	
-		$('#map div').removeClass('activeLand');
+	var nodeID;
+	$("#cpy_map .activeLand").on("mouseover", function() {
+	    nodeID = $( this ).prop('id');
+	    nodeID = nodeID.substr(4);
+	    $('#'+nodeID).css('transform', 'scale(1.15, 1.15)');
+	}).on("mouseout", function() {
+		$('#'+nodeID).css('transform', 'scale(1, 1)');
+	});
+
+	$('#cpy_map div').on('click', function() {
+		$('#cpy_map .inner').addClass('activeLand');	
+		$('#cpy_map div').removeClass('activeLand');
 		if ( !timeToChooseLand ) return;
 		var landID = $( this ).prop('id');
 		console.log( 'choosen #' + landID );
