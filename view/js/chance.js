a04cab53210f274ab779feb5021e9c2b10eeb5cf
c88tm <c88tm159357@gmail.com>
2017-07-10 22:45:26 +0800

    chance add player name

--- view/js/chance.js
+++ view/js/chance.js
@@ -1,5 +1,6 @@
 function showChance() {
-	$('#chanceBox').show();	
+	$('#chanceBox').show();
+	$('#chanceBox .des #chanceTitle').text(model.players[model.nowPlaying].name+"得到了機會!");
 	$('#chanceBox .des #chanceDescription').text(model.chance.description);
 	$('#chanceBox .des #chanceEffect').text("效果:"+model.chance.effect);
 }
