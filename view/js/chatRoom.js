e27299b7282a2458db229076e00e9ed47f6aaa1c
andy920262 <920262@gmail.com>
2017-07-11 20:04:59 +0800

    chatRoom stylesheet

--- view/js/chatRoom.js
+++ view/js/chatRoom.js
@@ -1,10 +1,10 @@
-$('form').submit(() =>{
+function sendMessage() {
 	var msg = $('#messageForm').val();
 	if(!admin)msg = model.players[playerId].name+" 說: "+msg;
 	socket.emit('chat_message',msg);
 	$('#messageForm').val('');
 	return false;
-	});
+}
 socket.on('chat_message',(msg) => {
-	$('#messages').append($('<li>').text(msg));
-	});
+	$('#messages').prepend($('<li>').text(msg));
+});
