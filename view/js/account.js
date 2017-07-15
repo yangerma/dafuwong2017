d799614f88440021161d3a4688b991b722f8180d
w4a2y4 <b04902079@ntu.edu.tw>
2017-07-15 15:36:03 +0800

    account.js

--- view/js/account.js
+++ view/js/account.js
@@ -1,11 +1,17 @@
 function showAccountBox( res ) {
 	
-	// read name
+
 	for (var i = 0; i < 5; i++) {
 		
 		$('#account' + i + ' .accountProf span').text( model.players[i].name );
+		$('#account' + i + ' .accountMoney span').text( Math.round10( model.players[i].money, -3 ) );
+		
+		/* TODO : read res */
+		$('#account' + i + ' .accountServer1 span').text(  );
+		$('#account' + i + ' .accountServer2 span').text(  );
+		$('#account' + i + ' .accountServer3 span').text(  );
 
-
+		$('#account' + i + ' .accountTotal span').text( Math.round10( , -3 ) );
 
 	}
 
